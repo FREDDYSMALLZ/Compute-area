6f2f06b255ed146ae0171b76c496103b7fcfb735
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-05 05:53:23 -0600

    Signed-off-by: Fredrick Ondieki <freddysmallz@gmail.com>

---
+++ ComputeArea/src/ComputeArea.java
@@ -0,0 +1,23 @@
+/**Computing the area of a circle
+ * The radius of the circle is 20 units and pie 3.14159
+ */
+
+/**
+ * @author fredd
+ *
+ */
+public class ComputeArea {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double radius;
+double area;
+radius = 20;
+area = radius*radius*3.14159;
+System.out.println("The area for the circle of radius " + radius   + " is "  +  area );
+	}
+
+}
